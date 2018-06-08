2d93e7154ed9dfc3ff8f6dfa2b0760cebc9befa9
Carlos Benavides <cebenavides@uninorte.edu.co>
2018-06-08 10:41:31 +0200

    Changes made to install dependencies in HypriotOS

--- deps_config.sh
+++ deps_config.sh
@@ -1,35 +1,39 @@
 #!/bin/sh
 set -e
 
+# Define the type of node being set (the selection could use regexp)
+read -r -p "Is this a Master or Slave node? (s) " node_choice
+case "$node_choice" in
+  m|M ) node_type=master;;
+  s|S ) node_type=slave;;
+  * ) node_type=slave;;
+esac
+
 # Install Golang
-wget -q https://dl.google.com/go/go1.10.2.linux-armv6l.tar.gz
-sudo tar -C /usr/local -xzf go1.10.2.linux-armv6l.tar.gz
-rm go1.10.2.linux-armv6l.tar.gz
-echo "export PATH=\$PATH:/usr/local/go/bin" >> .profile
+# wget https://dl.google.com/go/go1.10.2.linux-armv6l.tar.gz
+# sudo tar -C /usr/local -xzf go1.10.2.linux-armv6l.tar.gz
+# rm go1.10.2.linux-armv6l.tar.gz
+# echo "export PATH=\$PATH:/usr/local/go/bin" >> .profile
 
-# Install Docker
-sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
-echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
-     $(lsb_release -cs) stable" | \
-     sudo tee /etc/apt/sources.list.d/docker.list
-sudo apt-get update; sudo apt-get install -y docker-ce=18.04.0~ce~3-0~raspbian
-sudo systemctl start docker
-sudo usermod pi -aG docker
+# Add user to Docker group
+if [[ "$node_type" == "m" ]]; then
+  sudo usermod pirate -aG docker
+else
+  echo "Docker should be installed"
+  exit 1
+fi
 
-# Disable Swap
-sudo dphys-swapfile swapoff && \
-  sudo dphys-swapfile uninstall && \
-  sudo update-rc.d dphys-swapfile remove
-
-# Modify cmdline.txt
-echo Adding " cgroup_enable=cpuset cgroup_enable=memory" to /boot/cmdline.txt
+# Backup cmdline.txt and add cgroups
 sudo cp /boot/cmdline.txt /boot/cmdline_backup.txt
-orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_enable=memory"
+orig="$(head -n1 /boot/cmdline.txt) cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory"
 echo $orig | sudo tee /boot/cmdline.txt
 
-# Add repo list and install kubeadm
+# Install Kubernetes 1.10.2-00
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add - && \
   echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list && \
   sudo apt-get update -q && \
-  sudo apt-get install -qy kubelet=1.10.2-00 kubectl=1.10.2-00 kubeadm=1.10.2-00
+  sudo apt-get install -qy kubeadm=1.10.2-00 kubectl=1.10.2-00 kubelet=1.10.2-00
+
+if [[ "$node_type" == "m" ]]; then
+  sudo sed -i '/KUBELET_NETWORK_ARGS=/d' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+fi
